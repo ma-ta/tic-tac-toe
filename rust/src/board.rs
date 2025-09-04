c15381a1d22a44ec9b115ba88acdbae73f35c16e
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-09-05 00:51:25 +0200

    board

---
+++ rust/src/board.rs
@@ -0,0 +1,128 @@
+use std::{
+    fmt,
+    ops::{Deref, DerefMut},
+};
+
+#[derive(Debug, Clone)]
+pub struct Board {
+    size: (usize, usize),
+    square: bool,
+    cells: Cells,
+}
+
+impl Board {
+    pub fn new(rows: usize, cols: usize) -> Result<Self, String> {
+        const MIN_SIZE: (usize, usize) = (1, 3);
+        if rows >= MIN_SIZE.0 && cols >= MIN_SIZE.1 {
+            Ok(Self {
+                size: (rows, cols),
+                square: rows == cols,
+                cells: Cells::new(rows, cols),
+            })
+        } else {
+            Err(
+                format!(
+                    "Board size must be at least {}x{}",
+                    MIN_SIZE.0, MIN_SIZE.1
+                )
+            )
+        }
+    }
+
+    pub fn get_size(&self) -> (usize, usize) {
+        self.size
+    }
+
+    pub fn is_empty(&self, row: usize, col: usize) -> bool {
+        if let Some(Cell::Empty) = self.get(row, col) {
+            true
+        }
+        else {
+            false
+        }
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> Option<Cell> {
+        let index = self.get_index(row, col);
+        
+        self.cells.get(index).copied()
+    }
+
+    pub fn set(&mut self, row: usize, col: usize, status: Cell) -> bool {
+        let index = self.get_index(row, col);
+
+        if let Some(cell) = self.cells.get_mut(index) {
+            *cell = status;
+            return true;
+        }
+        
+        false
+    }
+
+    pub fn print(&self) {
+        println!("{}", self);
+    }
+
+    fn get_index(&self, row: usize, col: usize) -> usize {
+        row * self.size.0 + col
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut out = String::new();
+
+        for row in 0..self.get_size().0 {
+            out += &format!("{}+", "+----".repeat(self.get_size().0));
+            out += "\n|";
+        for col in 0..self.get_size().1 {
+            out += &format!(" {} |", self.get(row, col).unwrap());
+        }
+        out += "\n";
+    }
+        out += &format!("{}+", "+----".repeat(self.get_size().0));
+
+        write!(f, "{out}")
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Cell {
+    Empty,
+    Player1, // X
+    Player2, // O
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let symbol = match self {
+            Cell::Empty => '\u{3000}',
+            Cell::Player1 => '⚪', // X (⚪)
+            Cell::Player2 => '⚫', // O (⚫)
+        };
+        write!(f, "{}", symbol)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Cells(Vec<Cell>);
+
+impl Cells {
+    fn new(rows: usize, cols: usize) -> Self {
+        Self(vec![Cell::Empty; rows * cols])
+    }
+}
+
+impl Deref for Cells {
+    type Target = Vec<Cell>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Cells {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
---
+++ rust/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(dead_code)]
+
+mod board;
+
+use board::*;
+
+fn main() -> Result<(), String> {
+    const BOARD_SIZE: usize = 4;
+
+    let mut board =
+        Board::new(BOARD_SIZE, BOARD_SIZE)?;
+
+    board.set(0, 0, Cell::Player2);
+    board.set(1, 2, Cell::Player2);
+    board.set(3, 3, Cell::Player1);
+
+    board.print();
+
+    Ok(())
+}
