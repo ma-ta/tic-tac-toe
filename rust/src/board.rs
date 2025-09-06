3f1a0ee9006e9d8e0882f3202e927a2eddc42582
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-09-06 10:30:36 +0200

    bugs

--- rust/src/board.rs
+++ rust/src/board.rs
@@ -45,6 +45,9 @@ impl Board {
         self.cells.get(index).copied()
     }
 
+    // TODO: nesmí fungovat na obsazená políčka apod.
+    //       následně implementovat ++/-- volných políček v Board{}
+    //       asi vracet Result
     pub fn set(&mut self, row: usize, col: usize, status: Cell) -> bool {
         let index = self.idx(row, col);
 
@@ -68,27 +71,24 @@ impl Board {
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         const SYMBOLS: (char, char, char) = ('\u{3000}', '⚪', '⚫');
-        let mut out = String::new();
         let size = self.get_size();
 
         for row in 0..size.0 {
-            out += &format!("{}+", "+----".repeat(size.1));
-            out += "\n|";
+            write!(f, "{}+\n|", "+----".repeat(size.1))?;
             for col in 0..size.1 {
-                out += &format!(
+                write!(
+                    f,
                     " {} |",
                     match self.get(row, col).unwrap_or(Cell::Empty) {
                         Cell::Empty => SYMBOLS.0,
                         Cell::Player1 => SYMBOLS.1,
                         Cell::Player2 => SYMBOLS.2
                     }
-                );
+                )?;
             }
-            out += "\n";
+            writeln!(f)?;
         }
-        out += &format!("{}+", "+----".repeat(self.size.1));
-
-        write!(f, "{out}")
+        write!(f, "{}+", "+----".repeat(self.size.1))
     }
 }
 
