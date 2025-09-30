3f91632ba1ba770d54c8a9f2550f49850a83af4e
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-09-30 11:17:18 +0200

    test

--- rust/src/board.rs
+++ rust/src/board.rs
@@ -231,6 +231,34 @@ impl Board {
         println!("{out}");
     }
 
+    /// Some simple tests
+    pub fn test(size: (usize, usize)) -> Result<(), String> {
+        let mut _print_setup = PrintSetup::default();
+        let mut board =
+            match Board::new(size.0, size.1) {
+                Ok(board) => board,
+                Err(_) => return Err("Board::Error".to_string())
+            };
+        let mut count = 0;
+        for row in 0..board.get_size().0 {
+            for col in 0..board.get_size().1 {
+                board.set(row, col, Cell::Player(count));
+                count += 1;
+            }
+        }
+
+        println!("{board:?}");
+
+        let rows = board.all_rows();
+        let cols = board.all_cols();
+        let diags = board.all_diags();
+        println!("rows:\n{rows:?}");
+        println!("cols:\n{cols:?}");
+        println!("diags:\n{diags:?}");
+
+        Ok(())
+    }
+
     /* PRIVATE */
 
     /// Converts a (row, col) pair into a 1D index.
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,32 +4,6 @@ mod board;
 
 use board::*;
 
-fn main() -> Result<(), String> {
-    const BOARD_SIZE: usize = 4;
-    let mut _print_setup = PrintSetup::default();
-
-    let mut board =
-        match Board::new(BOARD_SIZE, BOARD_SIZE) {
-            Ok(board) => board,
-            Err(_) => return Err("Board::Error".to_string())
-        };
-
-    let mut count = 0;
-    for row in 0..board.get_size().0 {
-        for col in 0..board.get_size().1 {
-            board.set(row, col, Cell::Player(count));
-            count += 1;
-        }
-    }
-
-    println!("{board:?}");
-
-    let rows = board.all_rows();
-    let cols = board.all_cols();
-    let diags = board.all_diags();
-    println!("rows:\n{rows:?}");
-    println!("cols:\n{cols:?}");
-    println!("diags:\n{diags:?}");
-
-    Ok(())
+fn main() {
+    
 }
